02226292eb0bf4f181794e37b219b1b55edd475a
petcat <catanapetru@gmail.com>
2017-11-23 11:13:38 +0100

    added details in RentalDetails * Now you can see the details in larger size and also the booking menu on the right *added "Places near by"

--- sem3front_end/src/pages/RentalDetails.js
+++ sem3front_end/src/pages/RentalDetails.js
@@ -30,7 +30,7 @@ export default class RentalDetails extends React.Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-md-4">
+                    <div className="col-sm-8 col-md-8">
                         <div className="thumbnail">
                             <img src={place.imageUrl} />
                             <div className="caption">
@@ -41,10 +41,36 @@ export default class RentalDetails extends React.Component {
                                 <p>Rating: {place.rating} </p>
                             </div>
                         </div>
-                    </div>                    
+                    </div>
+                    <div className="col-sm-4 col-md-4">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">Book Your Holiday</div>
+                            <div className="panel-body">
+                                <p className="line">Choose your time frame: </p>
+                                <input id="date" type="date" />
+                                <input className="btn btn-success btn-block theLine register" type="button" name="Book" value="Book" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="page-header">
+                        <h1>Places near by </h1>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-6 col-md-4">
+                            <div className="thumbnail">
+                                <img src={place.imageUrl} alt="..." />
+                                <div className="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a  className="btn btn-primary" role="button">Button</a> <a  className="btn btn-default" role="button">Button</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
         )
     }
 }
