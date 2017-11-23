d36093da1b02559f73bf2148a4460a3399271cd7
TheLegendOfProgramming <buracmircea@yahoo.com>
2017-11-23 20:41:48 +0100

    Fine tuning css classes and html

--- sem3front_end/src/pages/RentalDetails.js
+++ sem3front_end/src/pages/RentalDetails.js
@@ -28,37 +28,37 @@ export default class RentalDetails extends React.Component {
     render() {
         const place = this.state.place;
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-8 col-md-8">
-                        <div className="thumbnail">
-                            <img src={place.imageUrl} />
-                            <div className="caption">
-                                <h1>{place.city}</h1>
-                                <p>Description: {place.description} </p>
-                                <p>Address: {place.address} </p>
-                                <p>Zip: {place.zip} </p>
-                                <p>Rating: {place.rating} </p>
+            <div className="container custom-container">
+                <div className="row sub-container">
+                    <div className="col-xs-4">
+                        <div className="row">
+                            <div className="thumbnail">
+                                <img src={place.imageUrl} />
+                                <div className="caption">
+                                    <h1>{place.city}</h1>
+                                    <p>Description: {place.description} </p>
+                                    <p>Address: {place.address} </p>
+                                    <p>Zip: {place.zip} </p>
+                                    <p>Rating: {place.rating} </p>
+                                </div>
+                            </div>
+                        </div>
+                            <div className="row">
+                            <div className="panel panel-default">
+                                <div className="panel-heading">Book Your Holiday</div>
+                                <div className="panel-body">
+                                    <p className="line">Choose your time frame: </p>
+                                    <input id="date" type="date" />
+                                    <input className="btn btn-success btn-block theLine register" type="button" name="Book" value="Book" />
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-sm-4 col-md-4">
-                        <div className="panel panel-default">
-                            <div className="panel-heading">Book Your Holiday</div>
-                            <div className="panel-body">
-                                <p className="line">Choose your time frame: </p>
-                                <input id="date" type="date" />
-                                <input className="btn btn-success btn-block theLine register" type="button" name="Book" value="Book" />
-                            </div>
+                    <div className="col-xs-8 nearby-places-container">
+                        <div className="page-header row">
+                            <h1>Places near by </h1>
                         </div>
-                    </div>
-                </div>
-                <div>
-                    <div className="page-header">
-                        <h1>Places near by </h1>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-6 col-md-4">
+                        <div className="row">
                             <div className="thumbnail">
                                 <img src={place.imageUrl} alt="..." />
                                 <div className="caption">
