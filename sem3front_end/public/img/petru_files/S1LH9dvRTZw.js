f4e150b75d173d8ffd8c45ed5b33dcdda409758e
TheLegendOfProgramming <buracmircea@yahoo.com>
2017-11-27 11:40:43 +0100

    Added new about picture * Added pic * Changed css a bit * Removed unused files from img folder

--- sem3front_end/public/img/petru_files/S1LH9dvRTZw.js
+++
@@ -1,3 +0,0 @@
-if (self.CavalryLogger) { CavalryLogger.start_js(["0\/lK8"]); }
-
-__d("CollabCallTriggerType",[],(function a(b,c,d,e,f,g){f.exports=Object.freeze({MEETING_ROOM_PAGE_PROFILE:"meeting_room_page_profile",QUICK_DIAL:"quickdial"})}),null);
