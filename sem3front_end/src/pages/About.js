0156aa113867787f563535b952ec433ab78be823
TheLegendOfProgramming <buracmircea@yahoo.com>
2017-11-27 12:05:42 +0100

    Moved about us to about section in different component

--- sem3front_end/src/pages/About.js
+++ sem3front_end/src/pages/About.js
@@ -1,26 +1,38 @@
 import React, {Component} from "react"
 import axios from 'axios'
 
-async function fetchAboutData(obj) {
-  	try {
-  	  const aboutData = await axios('https://baconipsum.com/api/?type=meat-and-filler')
-  	  // console.log(aboutData.data)
-  	  obj.state.data = aboutData.data
-  	} catch (e) {
-     console.error(e)
-    }
-}
+
 
 export default class About extends Component {
 
-  constructor() {
-    super();
-	this.state = { data: [] }
-	fetchAboutData(this)
-	console.log(this.state.data)
-  }
-
   render() {
-    return <h1>About<ul>{this.state.data}</ul></h1>
+    return (
+          <section id="about">
+            <div className="container wow fadeInUp">
+              <div className="row">
+                <div className="col-md-12">
+                  <h3 className="section-title">About Us</h3>
+                  <div className="section-title-divider"></div>
+                  <p className="section-description">We are a team of passionate developers determined to make this app, no matter how useless and confusing the specifications are</p>
+                </div>
+              </div>
+            </div>
+            <div className="container about-container wow fadeInUp">
+              <div className="row">
+                <div className="col-md-6 col-md-push-6 about-content">
+                  <h2 className="about-title">We provide great services and ideas</h2>
+                  <p className="about-text">
+                    We the bess
+                  </p>
+                  <p className="about-text">
+                    We makin' dough
+                  </p>
+                  <p className="about-text">
+                    We got you covered on that rental game tho
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>)
   }
 }
